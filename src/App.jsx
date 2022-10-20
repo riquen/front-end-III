af5026bd92f6babb75f412003d2c1524703ffe84
Henrique Nunes <79033028+riquen@users.noreply.github.com>
2022-10-19 22:57:08 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { SegundaAula } from "./aulas/SegundaAula"
+
+function App() {
+
+  return (
+    <div>
+      <SegundaAula />
+    </div>
+  )
+}
+
+export default App
---
+++ src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,40 @@
+import './style.scss'
+
+export function SegundaAula(){
+    return (
+        <main>
+            <h1>Segunda Aula</h1>
+            <h2>Projeto escolhido: Youtube</h2>
+            <ul>
+                <li>
+                    <h1>Primeiro componente</h1>
+                    <p>Header</p>
+                </li>
+                <li>
+                    <h1>Segundo componente</h1>
+                    <p>Ícone de Menu</p>
+                </li>
+                <li>
+                    <h1>Terceiro componente</h1>
+                    <p>Logo</p>
+                </li>
+                <li>
+                    <h1>Quarto componente</h1>
+                    <p>Input de Pesquisa</p>
+                </li>
+                <li>
+                    <h1>Quinto componente</h1>
+                    <p>Ícone de Teclado</p>
+                </li>
+                <li>
+                    <h1>Sexto componente</h1>
+                    <p>Botão de Pesquisa</p>
+                </li>
+                <li>
+                    <h1>Sétimo componente</h1>
+                    <p>Ícone de Microfone</p>
+                </li>
+            </ul>
+        </main>
+    )
+}
